67e1a1803245f172c67da2615458269438d906a0
Somayeh <mail.somayeh@gmail.com>
2018-06-11 14:27:55 +0100

    Create roman_numerals.rb file with RomanNumerals class

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,6 +1,10 @@
-describr RomanNumerals do
+require 'roman_numerals'
+describe RomanNumerals do
   subject(:romannumerals) { described_class.new }
+
   describe '#convert' do
-    expect(romannumerals.convert(1)).to eq 'I'
+    it 'should be able to convert 1 to I' do
+      expect(romannumerals.convert(1)).to eq 'I'
+    end
   end
 end

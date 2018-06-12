f1b704b56877ef2f451ae2bcfbaa059abba9ee8b
Somayeh <mail.somayeh@gmail.com>
2018-06-12 09:47:08 +0100

    Test to return combination of characters

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -7,12 +7,16 @@ describe RomanNumerals do
       expect(romannumerals.convert(1)).to eq 'I'
     end
 
-    it 'should be able to convert 5 to v' do
+    it 'should be able to convert 5 to V' do
       expect(romannumerals.convert(5)).to eq 'V'
     end
 
-    it 'should be able to convert 10 to x' do
+    it 'should be able to convert 10 to X' do
       expect(romannumerals.convert(10)).to eq 'X'
+    end
+
+    it 'should be able to convert 17 to XVII' do
+      expect(romannumerals.convert(17)).to eq 'XVII'
     end
   end
 end

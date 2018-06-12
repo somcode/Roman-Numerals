07848356a97fdefddde57237e6fd755e0dfbe476
Somayeh <mail.somayeh@gmail.com>
2018-06-12 12:21:58 +0100

    Refactor the code to pass all tests

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,6 +1,10 @@
 class RomanNumerals
   ROMAN_NUMBERS = { 10 => 'X', 5 => 'V' , 1 => 'I' }
   def convert(number)
-    ROMAN_NUMBERS[number]
+    result = ''
+    ROMAN_NUMBERS.each do |k, v|
+      (number / k).times { result << v; number = number - k }
+    end
+    result
   end
 end

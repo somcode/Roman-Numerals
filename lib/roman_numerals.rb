951895bb039afa84e57f5db9bf6fae82c7224954
Somayeh <mail.somayeh@gmail.com>
2018-06-12 12:46:08 +0100

    Add more pairs to ROMAN_NUMBERS hash to pass the test

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,5 +1,9 @@
 class RomanNumerals
-  ROMAN_NUMBERS = { 10 => 'X', 5 => 'V' , 1 => 'I' }
+  ROMAN_NUMBERS = { 1000 => 'M', 900 => 'CM',
+     500 => 'D', 400 => 'CD', 100 => 'C',
+     90 => 'XC', 50 => 'L', 40 => 'XL',
+     10 => 'X', 9 => 'IX', 5 => 'V',
+      4 => 'IV', 1 => 'I' }
   def convert(number)
     result = ''
     ROMAN_NUMBERS.each do |k, v|
